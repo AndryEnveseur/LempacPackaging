ffb64753de9b6369613c55cea1f676f7ce6d8891
AndryEnveseur <91246310+AndryEnveseur@users.noreply.github.com>
2023-03-09 16:00:45 +0100

    La animación del header ha sido cambiada
    Solo a nivel de código, realmente funciona igual que antes

--- headerScrollAnim.js
+++ headerScrollAnim.js
@@ -1,27 +1,33 @@
 const header = document.querySelector('.header-sticky');
+const main = document.querySelector('main');
 const HEADER_HEIGHT = 218.53;
 
 window.onscroll = e => {
     let scrollingToUp = this.oldScroll > this.scrollY;
 
     if (scrollingToUp) {
-        if (this.scrollY >= HEADER_HEIGHT * 2) {
-            header.classList.remove('header-sticky-hidden');
+        if (this.scrollY >= HEADER_HEIGHT) {
+            header.classList.remove('header-sticky-hide');
             header.classList.add('header-sticky-displayed');
         }
 
-        if (this.scrollY <= 0) {
-            header.style.opacity = 0;
+        if (this.scrollY <= 2) {
+            header.classList.remove('header-sticky-opacity-1');
+            header.classList.remove('header-sticky-displayed');
+            header.classList.add('header-sticky-hide');
         }
+
     //Scroll down
     } else {
+        header.classList.add('header-sticky-hide');
         header.classList.remove('header-sticky-displayed');
-        header.classList.add('header-sticky-hidden');
 
-        if (this.scrollY > HEADER_HEIGHT) {
-            header.style.opacity = 1;
+        if (this.scrollY >= HEADER_HEIGHT) {
+            header.classList.add('header-sticky-opacity-1');
         }
+
+
     }
 
-    this.oldScroll = this.scrollY; 
+    this.oldScroll = this.scrollY;
 }
