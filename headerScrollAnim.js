32a93f6e395aa2f798b88e4d8cd8b48487145508
AndryEnveseur <91246310+AndryEnveseur@users.noreply.github.com>
2023-03-13 15:03:59 +0100

    Sub menu para movil
    Ya casi está acabado. Quiero probarlo en un dispositivo móvil real

--- headerScrollAnim.js
+++ headerScrollAnim.js
@@ -1,9 +1,10 @@
+//#region | Header animation with scroll
 const getPropertyValue = (element, propertyName) => window.getComputedStyle(element).getPropertyValue(propertyName);
 
 const header = document.querySelector('header > div');
 const HEADER_HEIGHT = parseFloat(getPropertyValue(document.querySelector('header'), 'min-height').replace('px', ''));
 
-window.onscroll = e => {
+window.onscroll = () => {
     let scrollingToUp = this.oldScroll > this.scrollY;
 
     if (scrollingToUp) {
@@ -21,3 +22,21 @@ window.onscroll = e => {
 
     this.oldScroll = this.scrollY;
 }
+//#endregion
+
+const MEDIA_QUERY_MAX_WIDTH_900PX = 990;
+const headerTop = document.querySelector('header nav:first-of-type');
+const headerBottom = document.querySelector('header nav:last-of-type');
+const headerTopHeight = parseFloat(getPropertyValue(headerTop, 'min-height').replace('px', ''));
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= MEDIA_QUERY_MAX_WIDTH_900PX) {
+        headerBottom.style.height = `${(window.innerHeight - headerTopHeight)}px`;
+    } else {
+        headerBottom.style.height = 'auto';
+    }
+});
+
+/* If the hamburguer mobile menu is opened the user will not scroll vertically */
+const restrictScrollY = (e) => document.querySelector('body').style.overflowY = e.checked ? 'hidden' : 'auto';
+
