55061d1e464b4cb303358b9e08dd43dfc1723166
AndryEnveseur <91246310+AndryEnveseur@users.noreply.github.com>
2023-03-13 11:57:49 +0100

    Cambio animacion header y más
    Ahora la animación del header funciona diferente porque no me gustaba tener 2 headers. También he cambiado un poco el diseño de la página (espaciados)

--- headerScrollAnim.js
+++ headerScrollAnim.js
@@ -1,28 +1,22 @@
-const header = document.querySelector('header:first-of-type > div');
-const HEADER_HEIGHT = 218.53;
+const getPropertyValue = (element, propertyName) => window.getComputedStyle(element).getPropertyValue(propertyName);
+
+const header = document.querySelector('header > div');
+const HEADER_HEIGHT = parseFloat(getPropertyValue(document.querySelector('header'), 'min-height').replace('px', ''));
 
 window.onscroll = e => {
     let scrollingToUp = this.oldScroll > this.scrollY;
 
     if (scrollingToUp) {
-
         if (this.scrollY > HEADER_HEIGHT) {
-
             header.style.position = 'fixed';
             header.style.transform = `translateY(${0}px)`;
-
-        } else if (this.scrollY <= 1) {
-
+        } else if (this.scrollY <= 0) {
             header.style.position = 'initial';
-
         }
-
     } else {
-
         if (this.scrollY > HEADER_HEIGHT) {
             header.style.transform = `translateY(${-1 * HEADER_HEIGHT}px)`;
         }
-
     }
 
     this.oldScroll = this.scrollY;
