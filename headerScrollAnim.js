3cc24b462c5ec463d956d77b09971a6e3f9534cc
AndryEnveseur <91246310+AndryEnveseur@users.noreply.github.com>
2023-03-14 12:37:37 +0100

    Hambuguer menu casi hecho

--- headerScrollAnim.js
+++ headerScrollAnim.js
@@ -2,13 +2,20 @@
 const getPropertyValue = (element, propertyName) => window.getComputedStyle(element).getPropertyValue(propertyName);
 
 const header = document.querySelector('header > div');
+const input = document.querySelector('input#hamburguer-menu');
 const HEADER_HEIGHT = parseFloat(getPropertyValue(document.querySelector('header'), 'max-height').replace('px', ''));
-
-console.log(getComputedStyle(document.querySelector('body')).getPropertyValue('--header-top-height'));
 
 window.onscroll = (e) => {
 
     let scrollingToUp = this.oldScroll > this.scrollY;
+
+    //If the hamburguer menu is displayed, 
+    if (input.checked) {
+        header.style.position = 'fixed';
+        header.style.transform = `translateY(${0}px)`;
+        
+        return;
+    }
 
     if (scrollingToUp) {
         if (this.scrollY > HEADER_HEIGHT) {
@@ -18,7 +25,6 @@ window.onscroll = (e) => {
             header.style.position = 'initial';
         }
     } else {
-
         if (this.scrollY > HEADER_HEIGHT) {
             header.style.transform = `translateY(${-1 * HEADER_HEIGHT}px)`;
         }
@@ -28,12 +34,23 @@ window.onscroll = (e) => {
 }
 //#endregion
 
+const restrictScrollY = () => document.querySelector('body').classList.toggle('noscroll'); 
 
+const resizeHeightMenuMobile = () => document.querySelector('header .navbar-mobile').style.height = `${(window.innerHeight - HEADER_HEIGHT)}px;`;
 
-/* If the hamburguer mobile menu is opened the user will not scroll vertically */
-const restrictScrollY = (e) => document.querySelector('body').style.overflowY = e.checked ? 'hidden' : 'auto';
+// const MEDIA_QUERY_MAX_WIDTH_900PX = 990;
+// const headerTop = document.querySelector('header nav:first-of-type');
+// const headerBottom = document.querySelector('header nav:last-of-type');
+// const headerTopHeight = parseFloat(getPropertyValue(headerTop, 'min-height').replace('px', ''));
 
-const resizeHeightMenuMobile = () => {
-    console.log(HEADER_HEIGHT)
-    document.querySelector('header .navbar-mobile').style.height = `${(window.innerHeight - HEADER_HEIGHT)}px;`
-}
+// window.addEventListener('resize', () => {
+//     if (window.innerWidth <= MEDIA_QUERY_MAX_WIDTH_900PX) {
+//         headerBottom.style.height = `${(window.innerHeight - headerTopHeight)}px`;
+//         headerBottom.style.transitionDuration = '0ms';
+//         headerBottom.style.opacity = '0';
+
+//     } else {
+//         headerBottom.style.height = 'auto';
+//         headerBottom.style.opacity = '0';
+//     }
+// });
