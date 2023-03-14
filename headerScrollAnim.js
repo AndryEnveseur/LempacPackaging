84579e00634d32860526aed3c40e7681c64985d5
AndryEnveseur <91246310+AndryEnveseur@users.noreply.github.com>
2023-03-14 13:17:38 +0100

    Translucid background at hamburguer menu
    Now the translucid background also can close the hambuguer menu

--- headerScrollAnim.js
+++ headerScrollAnim.js
@@ -38,19 +38,9 @@ const restrictScrollY = () => document.querySelector('body').classList.toggle('n
 
 const resizeHeightMenuMobile = () => document.querySelector('header .navbar-mobile').style.height = `${(window.innerHeight - HEADER_HEIGHT)}px;`;
 
-// const MEDIA_QUERY_MAX_WIDTH_900PX = 990;
-// const headerTop = document.querySelector('header nav:first-of-type');
-// const headerBottom = document.querySelector('header nav:last-of-type');
-// const headerTopHeight = parseFloat(getPropertyValue(headerTop, 'min-height').replace('px', ''));
+const closeHamburguerMenu = () => {
+    const input = document.querySelector('input#hamburguer-menu');
+    const currentInputState = input.checked;
 
-// window.addEventListener('resize', () => {
-//     if (window.innerWidth <= MEDIA_QUERY_MAX_WIDTH_900PX) {
-//         headerBottom.style.height = `${(window.innerHeight - headerTopHeight)}px`;
-//         headerBottom.style.transitionDuration = '0ms';
-//         headerBottom.style.opacity = '0';
-
-//     } else {
-//         headerBottom.style.height = 'auto';
-//         headerBottom.style.opacity = '0';
-//     }
-// });
+    input.checked = !currentInputState;
+}
